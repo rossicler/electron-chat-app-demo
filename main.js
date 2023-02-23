f15630134db84d860e0255270dd03e6f209ee91a
Rossicler Junior <rossicler.junior@hostalky.com>
2023-02-23 17:32:35 -0500

    Basic electron setup

---
+++ main.js
@@ -0,0 +1,31 @@
+// Main process
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  // Browser window <- Renderer process
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    backgroundColor: "white",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile("index.html");
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
