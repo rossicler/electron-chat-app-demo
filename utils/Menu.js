ad6d82c62a78336de39f89000c2a75404e11a707
Rossicler Junior <rossicler.junior@hostalky.com>
2023-03-08 11:40:54 -0500

    Finished configuring builder and some small design changes

--- utils/Menu.js
+++ utils/Menu.js
@@ -17,7 +17,35 @@ exports.createTemplate = (app) => {
         {
           label: "Undo",
           accelerator: "CmdOrCtrl+Z",
-          role: "undo",
+          selector: "undo:",
+        },
+        {
+          label: "Redo",
+          accelerator: "Shift+CmdOrCtrl+Z",
+          selector: "redo:",
+        },
+        {
+          type: "separator",
+        },
+        {
+          label: "Cut",
+          accelerator: "CmdOrCtrl+X",
+          selector: "cut:",
+        },
+        {
+          label: "Copy",
+          accelerator: "CmdOrCtrl+C",
+          selector: "copy:",
+        },
+        {
+          label: "Paste",
+          accelerator: "CmdOrCtrl+V",
+          selector: "paste:",
+        },
+        {
+          label: "Select All",
+          accelerator: "CmdOrCtrl+A",
+          selector: "selectAll:",
         },
       ],
     },
